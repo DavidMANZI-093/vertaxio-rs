b8c9a1984b86920db0db4661affdd74b1dc69c78
DavidMANZI-093 <manzidavid093@gmail.com>
2026-03-07 17:51:06 +0200

    feat: add logger utility

---
+++ src/utils/logger.rs
@@ -0,0 +1,15 @@
+pub fn info(msg: &str) {
+    println!("vertaxio.\x1b[32minfo\x1b[0m - {}", msg);
+}
+
+pub fn debug(msg: &str) {
+    println!("vertaxio.\x1b[36mdebug\x1b[0m - {}", msg);
+}
+
+pub fn warn(msg: &str) {
+    eprintln!("vertaxio.\x1b[33mwarn\x1b[0m - {}", msg);
+}
+
+pub fn error(msg: &str) {
+    eprintln!("vertaxio.\x1b[31merror\x1b[0m - {}", msg);
+}
